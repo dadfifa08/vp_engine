51351f214b49fdb5908d6d31560433aaa7652ee8
David Armando Dominguez <dadfifa08@yahoo.com>
2025-12-08 12:54:33 -0600

    Fix UTF-8 encoding issue for Railway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 from scheduler.cron import run_engine
 
 if __name__ == '__main__':
-    print("?? TiraPalos Engine Starting (Tóxico Mode)…")
+    print("?? TiraPalos Engine Starting (Tï¿½xico Mode)ï¿½")
     run_engine()
