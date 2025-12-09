638a22cc4e9ca8dfa592630b0a8d20bffede9380
David Armando Dominguez <dadfifa08@yahoo.com>
2025-12-08 21:14:05 -0600

    Add professional logger module at root

---
+++ logger.py
@@ -0,0 +1,11 @@
+﻿import datetime
+import sys
+
+def _ts():
+    return datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+def log_info(message):
+    print(f"[INFO] {_ts()} {message}", file=sys.stdout, flush=True)
+
+def log_error(message):
+    print(f"[ERROR] {_ts()} {message}", file=sys.stderr, flush=True)
