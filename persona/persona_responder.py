e0e3b240935a7aab83a2b51685b98dbce0192e51
David Armando Dominguez <dadfifa08@yahoo.com>
2025-12-08 20:39:34 -0600

    Add persona responder module

---
+++ persona/persona_responder.py
@@ -0,0 +1,7 @@
+from logger import log_info
+
+class PersonaResponder:
+    def respond(self, text):
+        # placeholder
+        log_info("Persona responding...")
+        return "Respuesta tóxica placeholder #TiraPalos"
